3c7acbc82e283348b5f21d5ca3b0f4e98d52fd17
Sidnei Ramos <sidneiramos02@gmail.com>
2020-08-18 18:40:40 -0300

    PAges Configuracoes and Pagina Inicial

---
+++ src/pages/configuracoes.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class configuracoes extends Component {
+    render() {
+        return (
+            <header>
+                <h1 data-testid="settings-title">
+                    Configurações
+                </h1>
+            </header>
+        )
+    }
+}
