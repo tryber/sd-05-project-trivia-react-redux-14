394cb049e149f5c95c71f23a0fae22b962d92794
Sidnei Ramos <sidneiramos02@gmail.com>
2020-08-19 14:20:21 -0300

    CC Sem novidades

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,9 +4,9 @@ export default class header extends Component {
   render() {
     return (
       <div>
-        <img src={`https://www.gravatar.com/avatar/${hash}`} />
+        {/* <img src={`https://www.gravatar.com/avatar/${hash}`} />
         <p>Jogador: {nome}</p>
-        <p>Score: {placar}</p>
+        <p>Score: {placar}</p> */}
       </div>
     );
   }
