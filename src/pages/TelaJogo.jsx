a69b0a192a038d479681c35abb4c231d4bc0fad9
Sidnei Ramos <sidneiramos02@gmail.com>
2020-08-21 18:40:49 -0300

    Terceiro Requisito

--- src/pages/TelaJogo.jsx
+++ src/pages/TelaJogo.jsx
@@ -2,22 +2,41 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import { Redirect } from 'react-router-dom';
 
 export class TelaJogo extends Component {
   constructor(props) {
     super(props);
-    this.state = { count: 0 };
+    this.state = { count: 0, isClicked: false };
     this.nextQuestion = this.nextQuestion.bind(this);
+    this.clickC = this.clickC.bind(this);
+    this.clickI = this.clickI.bind(this);
   }
   // prettier-ignore
   nextQuestion() {
+    this.setState({isClicked: false});
     this.setState((previousState) => ({ count: previousState.count + 1 }));
     console.log(this.state.count);
   }
 
+  // prettier-ignore
+  clickC() {
+    this.setState({isClicked: true});
+    console.log(this.state.isClicked);
+  }
+
+  // prettier-ignore
+  clickI () {
+    this.setState({isClicked: true});
+    console.log(this.state.isClicked);
+  }
+
   render() {
     const { perguntas } = this.props;
-    const { count } = this.state;
+    const { count, isClicked } = this.state;
+    if (count > 4) {
+      return <Redirect to="/feedback" />;
+    }
     return (
       <div>
         <Header />
@@ -30,13 +49,27 @@ export class TelaJogo extends Component {
             <p data-testid="question-text">
               Question: {perguntas[count].question}
             </p>
-            <button data-testid="correct-answer">
+            <button
+              data-testid="correct-answer"
+              onClick={() => this.clickC()}
+              disabled={isClicked}
+              className={isClicked ? 'green-border' : null}
+            >
               {perguntas[count].correct_answer}
             </button>
             {perguntas[count].incorrect_answers.map((respostaI, i) => (
-              <button data-testid={`wrong-answer-${i}`}>{respostaI}</button>
+              <button
+                data-testid={`wrong-answer-${i}`}
+                onClick={() => this.clickI()}
+                disabled={isClicked}
+                className={isClicked ? 'red-border' : null}
+              >
+                {respostaI}
+              </button>
             ))}
-            <button onClick={() => this.nextQuestion()}>Next Question</button>
+            {isClicked && (
+              <button onClick={() => this.nextQuestion()}>Next Question</button>
+            )}
           </div>
         )}
       </div>
