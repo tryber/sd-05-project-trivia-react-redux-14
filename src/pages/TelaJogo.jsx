9783588a717930934279cc367cb4c0ffc6f03e73
Sidnei Ramos <sidneiramos02@gmail.com>
2020-08-21 16:35:37 -0300

    Add category and questions

--- src/pages/TelaJogo.jsx
+++ src/pages/TelaJogo.jsx
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { connect } from 'react-redux';
 
-export default class TelaJogo extends Component {
+export class TelaJogo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    this.nextQuestion = this.nextQuestion.bind(this);
+  }
+
+  nextQuestion() {
+    this.setState((previousState) => {
+      return { count: previousState.count + 1 };
+    });
+    console.log(this.state.count);
+  }
+
   render() {
+    const { perguntas, fetching } = this.props;
+    const { count } = this.state;
     return (
-      <header>
-        <h1>TELA DE JOGO</h1>
+      <div>
         <Header />
-      </header>
+        <h1>TELA DE JOGO</h1>
+        {perguntas[count] && (
+          <div>
+            <p data-testid="question-category">
+              Category: {perguntas[count].category}
+            </p>
+            <p data-testid="question-text">
+              Question: {perguntas[count].question}
+            </p>
+            <button onClick={() => this.nextQuestion()}>Next Question</button>
+          </div>
+        )}
+        {/* { perguntas[0] && <p>Question: {perguntas[0].question}</p> } */}
+      </div>
     );
   }
 }
+
+const mapStatetoProps = (state) => ({
+  perguntas: state.tokenReducer.questions,
+  fetching: state.tokenReducer.isFetching,
+});
+
+export default connect(mapStatetoProps)(TelaJogo);
