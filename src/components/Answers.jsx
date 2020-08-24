a0e794f196e7d4eac498eda7eb454a063d79e63c
Sidnei Ramos <sidneiramos02@gmail.com>
2020-08-24 20:06:26 -0300

    CC

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -91,7 +91,7 @@ class Answers extends Component {
     this.setState({
       player: {
         ...this.state.player,
-        score: this.state.player.score + (this.state.timer * dif + 10),
+        score: this.state.player.score + ((this.state.timer * dif) + 10),
         assertions: this.state.player.assertions + 1,
       },
     });
